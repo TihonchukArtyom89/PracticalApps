7ffde67b2577dc94ed4ffbfd6efc0b721c8d8b10
TihonchukArtyom89 <mortus1989@gmail.com>
2023-06-28 19:23:06 +0300

    work on model binding

--- Northwind.Mvc/Controllers/HomeController.cs
+++ Northwind.Mvc/Controllers/HomeController.cs
@@ -7,7 +7,7 @@ namespace Northwind.Mvc.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        private readonly NorthwindContext db;  
+        private readonly NorthwindContext db;
 
         public HomeController(ILogger<HomeController> logger, NorthwindContext injectedContext)
         {
@@ -17,7 +17,7 @@ namespace Northwind.Mvc.Controllers
 
         public IActionResult Index()
         {
-            HomeIndexViewModel model = new(VisitorCount:Random.Shared.Next(1,1001),Categories:db.Categories.ToList(),Products:db.Products.ToList());
+            HomeIndexViewModel model = new(VisitorCount: Random.Shared.Next(1, 1001), Categories: db.Categories.ToList(), Products: db.Products.ToList());
             return View(model);//pass model to view
         }
 
@@ -33,16 +33,27 @@ namespace Northwind.Mvc.Controllers
         }
         public IActionResult ProductDetail(int? id)
         {
-            if(!id.HasValue)
+            if (!id.HasValue)
             {
                 return BadRequest("You must pass a product ID in the route, for example, /Home/ProductDetail/21");
             }
             Product? model = db.Products.SingleOrDefault(p => p.ProductId == id);
-            if(model is null)
+            if (model is null)
             {
                 return NotFound($"ProductId {id} not found.");
             }
             return View(model);//pass model a view and then return result
         }
+        public IActionResult ModelBinding()
+        {
+            return View();//the page with form to submit
+        }
+        [HttpPost]//use this action method to process POSTs
+        public IActionResult ModelBinding(Thing thing)
+        {
+            HomeModelBindingViewModel model = new(Thing: thing, HasErrors: !ModelState.IsValid, ValidationErrors: ModelState.Values.SelectMany(state => state.Errors).Select(error => error.ErrorMessage));
+            return View(model);//show the model bound thing 
+        }
+
     }
 }
