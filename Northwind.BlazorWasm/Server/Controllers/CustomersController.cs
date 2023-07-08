8fd51205e0a93647c4953ccf95ba3cfb12776905
TihonchukArtyom89 <mortus1989@gmail.com>
2023-07-08 19:22:50 +0300

    begin work on configuring server for blazor webassembly (build customer web api controller) (Northwind.BlazorWaam.Server project) update readme.md

---
+++ Northwind.BlazorWasm/Server/Controllers/CustomersController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;//[ApiController],[Route]
+using Microsoft.EntityFrameworkCore;//ToListAsync,FirstOrDefaultAsync
+using Packt.Shared;//NorthwindContext,Customer
+
+namespace Northwind.BlazorWasm.Server.Controllers;
+[ApiController]
+[Route("api/[controller]")]
+public class CustomersController:ControllerBase
+{
+    private readonly NorthwindContext db;
+    public CustomersController(NorthwindContext db)
+    {
+        this.db = db;
+    }
+    [HttpGet]
+    public async Task<List<Customer>> GetCustomersAsync()
+    {
+        return await db.Customers.ToListAsync();
+    }
+    [HttpGet("in/{country}")]//different path to disambiguate
+    public async Task<List<Customer>> GetCustomersAsync(string country)
+    {
+        return await db.Customers.Where(c=>c.Country==country).ToListAsync();
+    }
+}
--- Northwind.BlazorWasm/Server/Program.cs
+++ Northwind.BlazorWasm/Server/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.ResponseCompression;
+using Packt.Shared;//AddNorthwindContext extension method
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
-
+builder.Services.AddNorthwindContext(relativePath:Path.Combine("..",".."));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
