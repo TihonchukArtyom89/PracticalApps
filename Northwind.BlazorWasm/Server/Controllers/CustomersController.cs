da38e666510e7d861b7898ae1170b8b04487337c
TihonchukArtyom89 <mortus1989@gmail.com>
2023-07-09 12:39:57 +0300

    end work on configuring server for blazor webassembly (build customer web api controller)

--- Northwind.BlazorWasm/Server/Controllers/CustomersController.cs
+++ Northwind.BlazorWasm/Server/Controllers/CustomersController.cs
@@ -22,4 +22,47 @@ public class CustomersController:ControllerBase
     {
         return await db.Customers.Where(c=>c.Country==country).ToListAsync();
     }
+    [HttpGet("{id}")]
+    public async Task<Customer?> GetCustomerAsync(string id)
+    {
+        return await db.Customers.FirstOrDefaultAsync(c => c.CustomerId == id);
+    }
+    [HttpPost]
+    public async Task<Customer?> CreateCustomerAsync(Customer customerToAdd)
+    {
+        Customer existing = await db.Customers.FirstOrDefaultAsync(c => c.CustomerId == customerToAdd.CustomerId);
+        if(existing==null)
+        {
+            db.Customers.Add(customerToAdd);
+            int affected = await db.SaveChangesAsync();
+            if(affected==1)
+            {
+                return customerToAdd;
+            }
+        }
+        return existing;
+    }
+    [HttpPut]
+    public async Task<Customer?>UpdateCustomerAsync(Customer c)
+    {
+        db.Entry(c).State= EntityState.Modified;
+        int affected = await db.SaveChangesAsync();
+        if (affected == 1)
+        {
+            return c;
+        }
+        return null;
+    }
+    [HttpDelete("{id}")]
+    public async Task<int> DeleteCustomerAsync(string id)
+    {
+        Customer c = await db.Customers.FirstOrDefaultAsync(c => c.CustomerId == id);
+        if (c!=null)
+        {
+            db.Customers.Remove(c);
+            int affected = await db.SaveChangesAsync();
+            return affected;
+        }
+        return 0;
+    }
 }
